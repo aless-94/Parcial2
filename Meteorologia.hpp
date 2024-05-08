4f0f07e7a8859c5b166e5469f0854645057bface
JuanMallamaci <110545355+JuanMallamaci@users.noreply.github.com>
2024-05-07 21:46:57 -0300

    Update Meteorologia.hpp

--- Meteorologia.hpp
+++ Meteorologia.hpp
@@ -1,17 +1,17 @@
 /*
- * Meteorologia.hpp
+ * DatClima.hpp
  *
  *  Created on: May 3, 2024
  *      Author: pc1
  */
 
-#ifndef METEOROLOGIA_HPP_
-#define METEOROLOGIA_HPP_
+#ifndef DATCLIMA_HPP_
+#define DATCLIMA_HPP_
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <fstream>
-#include <list>
+#include <vector>
 #include "Fecha.hpp"
 
 
@@ -22,35 +22,25 @@ private:
 	Fecha fecha;
 	float veloViento;
 	float temp;
+	float humedadRela ;
 	float mmH20;
 public:
 	DatClima ();
 	DatClima(const int& a, const int& m , const int& d);
 
+	Fecha GetFecha() const { return fecha; }
 	float GetTemp() const { return temp; }
-	float GetveloViento() const { return veloViento; }
+	float GetVeloViento() const { return veloViento; }
+	float GetHumedadRela() const { return humedadRela; }
 	float GetmmH20() const { return mmH20; }
 
+	void SetFecha(const Fecha& f) { fecha = f; }
 	void SetTemp(const float& t) { temp = t; }
-	void  SetveloViento(const float& v) { veloViento = v; }
+	void SetVeloViento(const float& v) { veloViento = v; }
+	void SetHumedadRela(const float& h) { humedadRela = h; }
 	void SetmmH20(const float& h) { mmH20 = h; }
 
 };
+
 std::ostream& operator<< (std::ostream& os , const DatClima& d);
-std::istream& operator>> (std::istream& in , const DatClima& d);
-class EstacionMet
-{
-	private:
-	std::list<DatClima> lectura;
-	public:
-	EstacionMet();
-
-
-
-
-};
-
-
-
-
-#endif /* METEOROLOGIA_HPP_ */
+std::istream& operator>> (std::istream& is , DatClima& d);
