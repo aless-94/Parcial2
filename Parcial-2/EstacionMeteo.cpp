f49829f8dde16085e3cd9c3c714dcfe595c14196
aless <aless@Debian12>
2024-05-13 23:05:00 -0300

    Mayo 13

--- Parcial-2/EstacionMeteo.cpp
+++ Parcial-2/EstacionMeteo.cpp
@@ -4,7 +4,39 @@
  *  Created on: May 7, 2024
  *      Author: pc1
  */
+#include "EstacionMeteo.hpp"
+#include "DatClima.hpp"
 
+EstacionMeteo::EstacionMeteo(float vel, float t, float mm, int a, int m, int d, int h, int mn)
+{
+
+	        DatClima nuevoDato;
+	        Fecha nuev;
+	        nuevoDato.SetveloViento(vel);
+	        nuevoDato.SetTemp(t);
+	        nuevoDato.SetmmH20(mm);
+	        nuev.SetAnio(a);
+	        nuev.SetMes(m);
+			nuev.SetDia(d);
+			nuev.SetHora(h);
+			nuev.SetMinuto(mn);
+			nuevoDato.SetFecha(nuev);
+	        datos.push_back(nuevoDato);
+	    }
+
+std::istream& operator>>(std::istream& in, EstacionMeteo& vec) {
+    DatClima dato;
+    char deter;
+    // Leer números del archivo hasta que se alcance el final del archivo
+    while (in >> dato) {
+    	vec.SetEstacionMeteo(dato);
+        in >> deter;
+        if (deter != '-') {
+        	throw 1;
+        }
+    }
+    return in;
+}
 
 
 
--- Parcial-2/EstacionMeteo.hpp
+++ Parcial-2/EstacionMeteo.hpp
@@ -5,19 +5,27 @@
  *      Author: pc1
  */
 
+
 #ifndef ESTACIONMETEO_HPP_
 #define ESTACIONMETEO_HPP_
+#include <vector>
+#include <iostream>
+#include <fstream>
 #include "DatClima.hpp"
-
 class EstacionMeteo
 {
 	private:
-	std::vector<DatClima> lectura;
+		std::vector<DatClima> datos;
 	public:
-	EstacionMeteo();
+		EstacionMeteo(){};
+		EstacionMeteo(float vel, float t, float mm, int a, int m, int d, int h, int mn);
 
+		void SetEstacionMeteo (const std::vector<DatClima>& date) {datos(date);}
+		void SetEstacionMeteo (const DatClima& date) {datos.push_back(date);}
 };
+std::istream& operator>>(std::istream& in,  EstacionMeteo& vec);
 
 
 
 #endif /* ESTACIONMETEO_HPP_ */
+
